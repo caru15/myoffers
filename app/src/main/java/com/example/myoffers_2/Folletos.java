76643c96a592a24fbc16b56bafe8f00bfa9d9fec
Alejandra <caru19785@gmail.com>
2021-03-18 10:07:25 -0300

    version 19

--- app/src/main/java/com/example/myoffers_2/Folletos.java
+++ app/src/main/java/com/example/myoffers_2/Folletos.java
@@ -1,24 +1,40 @@
-package com.example.myoffers_2;
+  package com.example.myoffers_2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
-public class Folletos extends Fragment {
+import com.hololo.tutorial.library.TutorialActivity;
+
+import static android.R.anim.slide_in_left;
+import static android.R.anim.slide_out_right;
+
+public class Folletos extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
+    private String dir;
+    private int i;
     private String mParam1;
     private String mParam2;
-
+    private WebView wv1;
+    private ViewFlipper viewFlipper;
+    private Boolean bandera;
     public Folletos() {
         // Required empty public constructor
     }
@@ -47,5 +63,51 @@ public class Folletos extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_folletos, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //int imagenes[]={R.drawable.vea1,R.drawable.vea2,R.drawable.vea3,R.drawable.vea4,R.drawable.vea5};
+        int imagenes[]=FolletosArgs.fromBundle(getArguments()).getImagenes();
+        viewFlipper = (ViewFlipper)view.findViewById(R.id.myflipper);
+        ImageButton btn1=(ImageButton)view.findViewById(R.id.btnAnterior);
+        btn1.setOnClickListener(this);
+        i=0;
+        //hago un boton start y stop
+        bandera=true;
+        while (bandera && i<imagenes.length){
+            FlipperImagen(imagenes[i]);
+            i=i+1;
+        }
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bandera){
+                btn1.setBackgroundResource(android.R.drawable.ic_media_play);
+                viewFlipper.stopFlipping();
+                bandera=false;
+                }else{
+                    btn1.setBackgroundResource(android.R.drawable.ic_media_pause);
+                    viewFlipper.startFlipping();
+                    bandera=true;
+                }
+            }
+        });
+
+    }
+
+    public void FlipperImagen(int imagen){
+    //aqui corre el flipper
+        ImageView ima= new ImageView(this.getContext());
+        ima.setBackgroundResource(imagen);
+        viewFlipper.addView(ima);
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.startFlipping();
+        viewFlipper.setInAnimation(this.getContext(),slide_in_left);
+        viewFlipper.setOutAnimation(this.getContext(),slide_out_right);
+    }
+    @Override
+    public void onClick(View v) {
     }
 }
--- app/src/main/java/com/example/myoffers_2/ofertasCercas.java
+++ app/src/main/java/com/example/myoffers_2/ofertasCercas.java
@@ -5,22 +5,32 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /***En esta clase lo que va son folletos de los supermercados que tiene ofertas***/
 
-public class ofertasCercas extends Fragment {
+public class ofertasCercas extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
+    private ImageButton imagen1;
+    private ImageButton imagen2;
+    private ImageButton imagen3;
+    private ImageButton imagen4;
+    private ImageButton imagen5;
+    private ImageButton imagen6;
+    private String url;
+    private String direccion;
     private String mParam1;
     private String mParam2;
     public ofertasCercas() {
@@ -56,32 +66,63 @@ public class ofertasCercas extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+         imagen1= (ImageButton) view.findViewById(R.id.imageButton2);
+        imagen2= (ImageButton)view.findViewById(R.id.imageButton);
+        imagen3=(ImageButton) view.findViewById(R.id.imageButton3);
+         imagen4=(ImageButton) view.findViewById(R.id.imageButton4);
+         imagen5=(ImageButton) view.findViewById(R.id.imageButton5);
+        imagen6=(ImageButton) view.findViewById(R.id.imageButton6);
+        imagen1.setOnClickListener(this);
+        imagen2.setOnClickListener(this);
+        imagen3.setOnClickListener(this);
+        imagen4.setOnClickListener(this);
+        imagen5.setOnClickListener(this);
+        imagen6.setOnClickListener(this);
     }
-    public void Vea(View v){
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageButton2://changoMas
+
+                break;
+            case R.id.imageButton://carrefour
+                int imagene[]={R.drawable.carrefour,R.drawable.carrefour1,R.drawable.carrefour2,R.drawable.carrefour3,R.drawable.carrefour4,R.drawable.carrefour5};
+                ofertasCercasDirections.ActionOfertasCercasToFolletos action=ofertasCercasDirections.actionOfertasCercasToFolletos(imagene);
+                action.setImagenes(imagene);
+                Navigation.findNavController(v).navigate(action);
+                break;
+            case R.id.imageButton3://Libertad
+
+                break;
+            case R.id.imageButton4://dia
+                 int ima[]={R.drawable.dia1,R.drawable.dia2};
+                 ofertasCercasDirections.ActionOfertasCercasToFolletos action4 = ofertasCercasDirections.actionOfertasCercasToFolletos(ima);
+                 action4.setImagenes(ima);
+                 Navigation.findNavController(v).navigate(action4);
+                break;
+            case R.id.imageButton5://vea
+                int imagenes[]={R.drawable.vea1,R.drawable.vea2,R.drawable.vea3,R.drawable.vea4,R.drawable.vea5};
+                ofertasCercasDirections.ActionOfertasCercasToFolletos action2=ofertasCercasDirections.actionOfertasCercasToFolletos(imagenes);
+                action2.setImagenes(imagenes);
+                Navigation.findNavController(v).navigate(action2);
+                break;
+            case R.id.imageButton6://Damesco
+                int imag[]={R.drawable.damesco1,R.drawable.damesco2,R.drawable.damesco3,R.drawable.damesco4};
+                ofertasCercasDirections.ActionOfertasCercasToFolletos action6=ofertasCercasDirections.actionOfertasCercasToFolletos(imag);
+                action6.setImagenes(imag);
+                Navigation.findNavController(v).navigate(action6);
+                break;
+            default:
+                
+        }
     }
-    public void Dia(View v){
 
-    }
-    public void Libertad(View v){
-     }
-
-    public void ChangoMas(View v){
-
-    }
-    public void Carrefour(View v){
-
-    }
-    public void Damesco(View v){
-
-       // i.putExtra("sitioweb","http://www.damesco.com.ar/ofertas");
-
-    }
-   /*** public boolean onCreateOptionsMenu(Menu menu){
+   /** public boolean onCreateOptionsMenu(Menu menu){
         super.onCreateOptionsMenu(menu);
         MenuItem item=menu.add("Ver Mapa");
         MenuItem item1=menu.add("Ingresar Productos");
         MenuItem item2=menu.add("Salir");
         return true;
-    }**/
+    }*/
 }
