757f70d4e74bd8ee52030763e4373fe23d3c0e91
Alejandra <caru19785@gmail.com>
2021-06-29 10:41:26 -0300

    version 42

--- app/src/main/java/com/example/myoffers_2/ruta.java
+++ app/src/main/java/com/example/myoffers_2/ruta.java
@@ -3,6 +3,8 @@ package com.example.myoffers_2;
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -18,8 +20,12 @@ import android.location.LocationProvider;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 //import com.google.android.gms.location.LocationListener;
 
@@ -53,6 +59,8 @@ public class ruta extends Fragment implements LocationListener {
     Double latitudOrigen, longitudOrigen;
     Double latitudDest, longitudDest;
     LatLng dest;
+    Toolbar toolbar;
+    private int[] prod=new int[2];
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -83,12 +91,11 @@ public class ruta extends Fragment implements LocationListener {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        OnBackPressedCallback onBackPressedCallback= new OnBackPressedCallback(true) {
-            @Override
-            public void handleOnBackPressed() {
-                Navigation.findNavController(view).navigate(R.id.resultBusqueda);
-            }
-        };
+/**
+      toolbar =(Toolbar) view.findViewById(R.id.idtool);
+        setHasOptionsMenu(true);
+        toolbar.inflateMenu(R.menu.main);
+        ((AppCompatActivity)getContext()).setSupportActionBar(toolbar);*/
 
         String lat=rutaArgs.fromBundle(getArguments()).getLatitud();
         latitudDest=Double.parseDouble(lat);
@@ -111,6 +118,30 @@ public class ruta extends Fragment implements LocationListener {
     }
 
     @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        {
+            inflater.inflate(R.menu.main, menu);
+        }
+    }
+ /**   @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        prod[0]=0;
+        prod[1]=0;
+        int i=item.getItemId();
+        if (i==R.id.idExit){
+        rutaDirections.ActionRutaToResultBusqueda action=rutaDirections.actionRutaToResultBusqueda(prod);
+        action.setLongitud("0");
+        action.setLatitud("0");
+        action.setDistancia(0);
+        action.setProductos(prod);
+        Navigation.findNavController(getView()).navigate(action);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    @Override
     public void onLocationChanged(@NonNull Location location) {
         LatLng mipos = new LatLng(location.getLatitude(), location.getLongitude());
        // map.moveCamera(CameraUpdateFactory.newLatLngZoom(mipos,14));
@@ -120,7 +151,7 @@ public class ruta extends Fragment implements LocationListener {
         map.addMarker(new MarkerOptions().position(dest).title("Super"));
         CameraPosition cameraPosition= new CameraPosition.Builder()
                 .target(new LatLng(location.getLatitude(),location.getLongitude()))
-                .zoom(16)
+                .zoom(17)
                 .build();
         map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
@@ -159,7 +190,7 @@ public class ruta extends Fragment implements LocationListener {
                          String polyline=""+((JSONObject)((JSONObject) JsonSteps.get(k)).get("polyline")).get("points");
                          Log.d("end"," "+polyline);
                          List<LatLng> list= PolyUtil.decode(polyline);
-                         map.addPolyline(new PolylineOptions().addAll(list).color(R.color.Rojo).width(9));
+                         map.addPolyline(new PolylineOptions().addAll(list).color(R.color.Rojo).width(10));
                      }
                  }
             }
@@ -188,10 +219,8 @@ public class ruta extends Fragment implements LocationListener {
     public void onProviderEnabled(String provider) {
 
     }
-
     @Override
     public void onProviderDisabled(String provider) {
 
     }
-
 }
